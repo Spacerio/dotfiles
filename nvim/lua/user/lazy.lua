b8645141c3893a7083a269a35fe371e1ed4f0b00
Oiva Laitinen <user@DESKTOP-CQFO475>
2023-01-16 22:08:48 +0200

    Add lazy.nvim as plugin manager

---
+++ nvim/lua/user/lazy.lua
@@ -0,0 +1,33 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
+end
+vim.opt.rtp:prepend(lazypath)
+
+plugins = {
+	-- the colorscheme should be available when starting Neovim
+	{
+		"folke/tokyonight.nvim",
+		lazy = false, -- make sure we load this during startup if it is your main colorscheme
+		priority = 1000, -- make sure to load this before all the other start plugins
+		config = function()
+			-- load the colorscheme here
+			vim.cmd([[colorscheme tokyonight-night]])
+		end,
+
+	},
+
+	{ 'nvim-lua/plenary.nvim' },
+	{ 'nvim-telescope/telescope.nvim' },
+}
+
+--local plugins = require('user.plugins')
+
+require("lazy").setup(plugins, opts)
--- nvim/lua/user/remap.lua
+++ nvim/lua/user/remap.lua
@@ -1,5 +1,6 @@
 local opts = {noremap = true, silent = true}
 local map = vim.api.nvim_set_keymap
+vim.g.mapleader = " "
 
 --Buffer navigation
 map('n', 'L', ':bn<cr>', opts)
@@ -8,3 +9,12 @@ map('n', 'H', ':bp<cr>', opts)
 --Split navigation
 map('n', '<C-h>', '<C-w>h', opts)
 map('n', '<C-l>', '<C-w>l', opts)
+map('n', '<C-j>', '<C-w>j', opts)
+map('n', '<C-k>', '<C-w>k', opts)
+
+--Telescope
+themes = require('telescope.themes')
+builtin = require('telescope.builtin')
+compact_dropdown = themes.get_dropdown{previewer = false}
+
+map('n', '<C-p>', ":lua builtin.fd(compact_dropdown)<cr>", opts) 
