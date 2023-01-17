5a6c0c75569c9c81550deb02ee148c26782a996f
Oiva Laitinen <user@DESKTOP-CQFO475>
2023-01-17 18:03:01 +0200

    General progress

--- nvim/lua/user/color.lua
+++ nvim/lua/user/color.lua
@@ -1,1 +1,28 @@
---vim.cmd([[colorscheme tokyonight-night]])
+function color(color, transparent)
+	transparent = transparent or true
+	color = color or "tokyonight-night"
+
+	if transparent then
+		vim.api.nvim_set_hl(0, "Normal", {bg="none"})
+		vim.api.nvim_set_hl(0, "NormalFloat", {bg="none"})
+	end
+
+	if color == "tokyonight-night" then
+		require('tokyonight').setup({
+			transparent = transparent,
+			on_colors = function(colors)
+				colors.bg_float = "none"
+			end
+		})
+
+		if transparent then 
+			vim.g.tokyonight_colors = {
+				bg_float = "none"
+			}
+		end
+	end
+	vim.cmd.colorscheme(color)
+end
+
+color()
+
--- nvim/lua/user/remap.lua
+++ nvim/lua/user/remap.lua
@@ -2,6 +2,7 @@ local opts = {noremap = true, silent = true}
 local remap = {noremap = false, silent = true}
 local map = vim.api.nvim_set_keymap
 
+map("", "<Space>", "<Nop>", opts)
 vim.g.mapleader = " "
 
 --Buffer navigation
@@ -14,12 +15,17 @@ map('n', '<C-l>', '<C-w>l', opts)
 map('n', '<C-j>', '<C-w>j', opts)
 map('n', '<C-k>', '<C-w>k', opts)
 
+--Source files wih keymap
+map('n', '<leader>s', ':so<cr>', opts)
+
+
 --Telescope
 themes = require('telescope.themes')
 builtin = require('telescope.builtin')
 compact_dropdown = themes.get_dropdown{previewer = false}
 
 map('n', '<C-p>', ":lua builtin.fd(compact_dropdown)<cr>", opts) 
+map('n', '<C-n>', ":Telescope builtin<cr>", opts)
 
 --Lazy.nvim
-map('n', '<leader>l', ':Lazy<cr>', remap)
+map('n', '<leader>l', ':Lazy<cr>', opts)
