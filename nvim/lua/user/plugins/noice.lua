1ea579034ded0fc20e2b053cbe22f104f57c0d7e
Oiva Laitinen <user@DESKTOP-CQFO475>
2023-01-19 21:43:25 +0200

    Add noice.nvim and some basic configuration

---
+++ nvim/lua/user/plugins/noice.lua
@@ -0,0 +1,13 @@
+require('noice').setup({
+	lsp = {
+		override = {
+			["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+			["vim.lsp.util.stylize_markdown"] = true,
+		},
+	},
+	presets = {
+		bottom_search = false,
+		command_palette = false,
+		long_message_to_split = true,
+	},
+})
