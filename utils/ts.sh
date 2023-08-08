d40e56c0af205abe2904ed457dc9496a42673ce6
OL <oiva.laitinen2@gmail.com>
2023-08-08 20:05:21 +0300

    Fix ts.sh filenames

--- utils/ts.sh
+++ utils/ts.sh
@@ -3,7 +3,7 @@
 if [[ $# -eq 1 ]]; then
     selected=$1
 else
-	selected=$(find ~ ~/projects ~/projects/codewars ~/projects/test -mindepth 0 -maxdepth 1 -type d | fzf)
+	selected=$(find ~ ~/projects/rust ~/projects/lua ~/projects/cpp ~/projects/test -mindepth 0 -maxdepth 1 -type d | fzf)
 fi
 
 if [[ -z $selected ]]; then
