2ab75dfaa581b9ee0fb0b1b02c95c344541079ff
Oiva Laitinen <oiva.laitinen2@gmail.com>
2024-03-01 20:53:35 +0200

    Update .wezterm.lua

--- .wezterm.lua
+++ .wezterm.lua
@@ -2,9 +2,18 @@ local wezterm = require("wezterm")
 
 local config = wezterm.config_builder()
 
--- config.color_scheme = 'Batman'
--- config.color_scheme = 'Batman'
-
+config.keys = {
+	{
+		key = 'B',
+		mods = "SHIFT|CTRL",
+		action = wezterm.action.ToggleFullScreen,
+	},
+	{
+		key = 'R',
+		mods = "SHIFT|CTRL",
+		action = wezterm.action.ReloadConfiguration
+	}
+}
 
 config.window_background_gradient = {
 	orientation = "Vertical",
@@ -19,7 +28,9 @@ config.window_decorations = "RESIZE"
 config.enable_scroll_bar = false
 config.hide_tab_bar_if_only_one_tab = true
 
+config.window_background_opacity = 0.8
 config.win32_system_backdrop = "Acrylic"
 
+config.default_domain = 'WSL:Ubuntu'
 
 return config
