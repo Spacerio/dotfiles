25be279788173c63b145a502813c089c575551be
OL <oiva.laitinen2@gmail.com>
2024-03-06 21:06:00 +0200

    Add alias.fish to utils

--- config.fish
+++ config.fish
@@ -12,32 +12,4 @@ if status is-interactive
 
 
     fish_add_path $HOME/.cargo/bin $HOME/.local/share/bob/nvim-bin
-
-    alias cls='clear'
-    alias winuser='cd /mnt/c/Users/Omistaja'
-    alias br='br -h'
-    alias e='exit'
-    alias alacritty='cd /mnt/c/Users/Omistaja/AppData/Roaming/alacritty'
-    alias lg='lazygit'
-    alias t='tmux'
-    alias ta='tmux attach'
-    alias tconfig='nvim ~/.tmux.conf'
-    alias v='nvim'
-    alias stmux='tmux source ~/.tmux.conf'
-    alias ta='tmux a'
-    alias apexconfig='cd /mnt/c/Program Files (x86)/Steam/steamapps/common/Apex Legends/cfg'
-    alias ghclone='git clone https://github.com/' 
-    alias l='ls -CF'
-    alias la='ls -A'
-    alias ll='ls -alF'
-    alias ls='ls --color=auto'
-    alias sl="ls"
-    alias ..="cd .."
-    alias ...="cd ../.."
-    alias ....="cd ../../.."
-    alias s="sk --preview='bat {} --color=always'"
-    alias ns="nvim -c SessionsLoad"
-    alias so="exec zsh"
-    alias sk="rg --files -L -. | sk --bind 'ctrl-t:execute(nvim {})+abort' --preview 'bat {} --color=always'"
-    alias make="make -s"
 end
