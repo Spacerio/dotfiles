8e8e923665ddb7c27aca7eb4709af47ed94e733c
OL <oiva.laitinen2@gmail.com>
2024-03-13 20:21:07 +0200

    Finish joshuto.fish

--- utils/joshuto.fish
+++ utils/joshuto.fish
@@ -1,10 +1,12 @@
 #!/bin/env fish
 
-joshuto --file-chooser --output-file ~/.dotfiles/utils/temp
-set file (cat temp)
-set dir (path dirname < temp)
+set temp ~/.dotfiles/utils/temp
+
+joshuto --file-chooser --output-file $temp
+set file (cat $temp)
+set dir (path dirname < $temp)
 set name (path basename $dir | tr . _)
-rm ~/.dotfiles/utils/temp
+rm $temp
 
 if test -z $TMUX && test -z $tmux_running
     tmux new-session -s $name -c $dir
@@ -15,4 +17,5 @@ if not tmux has-session -t=$name 2> /dev/null
     tmux new-session -ds $name -c $dir
 end
 
-tmux switch-client -t $name
+tmux switch-client -t $name \; \
+	neww "nvim $file"
