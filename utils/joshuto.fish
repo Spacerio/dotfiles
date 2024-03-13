296b86e834b43e8b02e7bf2ca03702706cc4c70c
OL <oiva.laitinen2@gmail.com>
2024-03-13 19:52:17 +0200

    Update joshuto.fish

--- utils/joshuto.fish
+++ utils/joshuto.fish
@@ -1,3 +1,18 @@
 #!/bin/env fish
 
-joshuto --file-chooser --output-file temp
+joshuto --file-chooser --output-file ~/.dotfiles/utils/temp
+set file (cat temp)
+set dir (path dirname < temp)
+set name (path basename $dir | tr . _)
+rm ~/.dotfiles/utils/temp
+
+if test -z $TMUX && test -z $tmux_running
+    tmux new-session -s $name -c $dir
+    exit 0
+end
+
+if not tmux has-session -t=$name 2> /dev/null
+    tmux new-session -ds $name -c $dir
+end
+
+tmux switch-client -t $name
