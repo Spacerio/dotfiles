82d0f0a25ac9fb07cfa07a347a2a5f7d45b8a05d
OL <oiva.laitinen2@gmail.com>
2024-03-13 22:13:29 +0200

    Update yazi config and yazi.fish

---
+++ utils/yazi.fish
@@ -0,0 +1,27 @@
+#!/bin/env fish
+
+set wd ~/.dotfiles
+set temp ~/.dotfiles/utils/temp
+
+yazi $wd --chooser-file $temp
+if ! test -f $temp
+	exit 0
+end
+set dir (cat $temp)
+set name (path basename $dir | tr . _)
+rm $temp
+
+if ! test -d $dir
+	set dir (path dirname $dir)
+end
+
+if test -z $TMUX && test -z $tmux_running
+    tmux new-session -s $name -c $dir
+    exit 0
+end
+
+if not tmux has-session -t=$name 2> /dev/null
+    tmux new-session -ds $name -c $dir
+end
+
+tmux switch-client -t $name
