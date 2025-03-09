8cf4d0d74f4e8a7e9fd352835c5d8dc8e9b60081
Oiva Laitinen <oiva.laitinen2@gmail.com>
2025-03-09 13:40:35 +0200

    Update hyprland config for using rider

--- hypr/gamemode.sh
+++ hypr/gamemode.sh
@@ -5,10 +5,11 @@ if [ "$HYPRGAMEMODE" = 1 ] ; then
         keyword animations:enabled 0;\
         keyword decoration:shadow:enabled 0;\
         keyword decoration:blur:enabled 0;\
+        keyword decoration:dim_inactive 0;\
         keyword general:gaps_in 0;\
         keyword general:gaps_out 0;\
         keyword general:border_size 1;\
-        keyword decoration:rounding 0"
+        keyword decoration:rounding 0";\
     exit
 fi
 hyprctl reload
