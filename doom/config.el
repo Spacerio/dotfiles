f200148d853510039813068545b62901ccc75445
Oiva Laitinen <oiva.laitinen2@gmail.com>
2025-04-17 20:26:54 +0300

    Update emacs config

--- doom/config.el
+++ doom/config.el
@@ -76,8 +76,10 @@
 ;; they are implemented.
 
 (setq! scroll-margin 10)
-(setq tab-width 4)
-(setq evil-shift-width 4)
+(setq-default tab-width 4)
+(setq! evil-shift-width 4)
+(setq! which-key-idle-delay 0.1)
+(setq doom-font (font-spec :size 16))
 
 ;; enter calc already in emacs mode
 (add-hook 'calc-mode-hook 'evil-emacs-state)
@@ -105,3 +107,55 @@
            "2 sec" nil 'delete-windows-on
            (get-buffer-create "*compilation*"))
           (message "No Compilation Errors!")))))
+
+;; Org config
+(after! org
+  (setq org-directory "~/org/")
+  (setq org-agenda-files '("~/org/agenda.org" "~/org/todo.org"))
+  (setq org-roam-directory "~/notes/")
+)
+;; Custom templates to disable automatic linking to current file
+(setq org-capture-templates
+      '(("t" "Personal todo" entry (file+headline +org-capture-todo-file "Inbox")
+         "* [ ] %?\n%i" :prepend nil)
+        ("n" "Personal notes" entry (file+headline +org-capture-notes-file "Inbox")
+         "* %u %?\n%i" :prepend t)
+        ("j" "Journal" entry (file+olp+datetree +org-capture-journal-file)
+         "* %U %?\n%i" :prepend t)
+
+        ("c" "Templates with link to current file")
+        ("ct" "Capture todo" entry (file+headline +org-capture-todo-file "Inbox")
+         "* [ ] %?\n %i\n %a" :prepend nil)
+        ("cn" "Capture notes" entry (file+headline +org-capture-notes-file "Inbox")
+         "* %u %?\n %a\n %i" :prepend t)
+        ("cj" "Capture journal" entry (file+headline +org-capture-journal-file "Inbox")
+         "* %U %?\n %a\n %i" :prepend t)
+
+        ("p" "Templates for projects")
+        ("pt" "Project-local todo" entry
+         (file+headline +org-capture-project-todo-file "Inbox") "* TODO %?\n%i\n%a"
+         :prepend t)
+        ("pn" "Project-local notes" entry
+         (file+headline +org-capture-project-notes-file "Inbox") "* %U %?\n%i\n%a"
+         :prepend t)
+        ("pc" "Project-local changelog" entry
+         (file+headline +org-capture-project-changelog-file "Unreleased")
+         "* %U %?\n%i\n%a" :prepend t)
+
+        ("o" "Centralized templates for projects")
+        ("ot" "Project todo" entry #'+org-capture-central-project-todo-file
+         "* TODO %?\n %i\n %a" :heading "Tasks" :prepend nil)
+        ("on" "Project notes" entry #'+org-capture-central-project-notes-file
+         "* %U %?\n %i\n %a" :heading "Notes" :prepend t)
+        ("oc" "Project changelog" entry #'+org-capture-central-project-changelog-file
+         "* %U %?\n %i\n %a" :heading "Changelog" :prepend t)
+        ))
+
+(setq org-roam-capture-templates
+      '(("d" "default" plain "%?"
+         :target (file+head "main/${slug}.org" "#+title: ${title}\n")
+         :unnarrowed t)
+        ("s" "school" entry "* %?"
+         :target (file+head "main/${slug}.org" "#+title: ${title}\n#+FILETAGS: :school:\n")
+         :unnarrowed t)
+))
